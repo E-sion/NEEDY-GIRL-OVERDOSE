562914ef1c654a2ddf9f3bf6437547eeefab7118
E-sion <101786678+E-sion@users.noreply.github.com>
2023-11-13 15:32:18 +0800

    适应文本框

--- new_chatlist/main.py
+++ new_chatlist/main.py
@@ -8,6 +8,8 @@ from ui.new_chatlist import Ui_MainWindow
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.height_size = None
+        self.width_size = None
         self.free_width = None
         self.setupUi(self)
         self.button = QPushButton(self)
@@ -30,11 +32,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.textBrowser.setObjectName(u"textBrowser")
         self.textBrowser.setText(text)
 
-        # 自适应
-        self.textBrowser.document().setTextWidth(0)
-        self.free_width = self.width() - self.textBrowser.x() - 220
-        self.textBrowser.setFixedSize(self.textBrowser.document().size().width() + self.free_width,
-                                      self.textBrowser.document().size().height() - 50)  # 设置控件的大小为文档的大小
+        # 自适应 todo 未来可以使用更好的方式来改变newHeight的计算方式，使其跟贴合文本内容
+        self.textBrowser.document().adjustSize()
+        newWidth = self.width_size / 3
+        newHeight = self.textBrowser.document().size().height()
+        # 已知高度大于56后会出现滑动条，
+        if newHeight >= 56:
+            # 重新处理高度
+            newHeight = newHeight*2
+
+        print(newWidth, '---', self.textBrowser.document().size().height())
+
+        # 设置屏幕的五分之三的宽度
+
+        if newWidth != self.textBrowser.width():
+            self.textBrowser.setFixedWidth(newWidth)
+        if newHeight != self.textBrowser.height():
+            self.textBrowser.setFixedHeight(newHeight)
 
         self.textBrowser.setStyleSheet(u".QTextBrowser {\n"
                                        "	font: 500 12pt \"Zpix\";\n"
@@ -53,12 +67,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.horizontalLayout.setContentsMargins(15, 15, 0, 15)
         return self.widget
 
+    def resizeEvent(self, event):
+        # 获取新的窗口大小
+        new_size = event.size()
+        self.width_size = new_size.width()
+        self.height_size = new_size.height()
+        # print("Window resized to:", new_size.width(), new_size.height())
+
     # 处理list
     def set_item(self):
         item = QListWidgetItem()  # 创建QListWidgetItem对象
         item.setSizeHint(QSize(200, 50))  # 设置QListWidgetItem大小
         # 设置self.widget右对齐 ： Qt.AlignRight
-        text = f'这是一段测试tasdhajsdhkahdjkashdkjashdkjashkashdkjashdkjashdkjasshdjkhasjkdhaskjdh'
+        text = (f'这是一段测试阿萨啊啊啊啊啊啊啊啊a啊啊啊啊a啊啊啊啊a啊啊啊啊a啊啊啊啊a啊啊啊啊a啊啊啊啊a啊啊啊啊a啊啊啊啊a啊a啊大')
         widget = self.get_item('../file/background/1.jpg', text)
         # 添加item
         self.listWidget.addItem(item)  # 添加item
